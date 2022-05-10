4721911382d8c4c256461694abef18ec54373523
DSAghicha <dsaghicha211@gmail.com>
2022-05-11 01:48:31 +0530

    Introduced Header

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,10 @@
+import { Header } from '../../components'
 import './Home.sass'
 
 export default function Home() {
     return(
         <>
+            <Header />
             <h1>Home Page</h1>
         </>
     )
